jra3/system-agent

commit 7d6098af0f6b29df9a6d5f6c250c90c2cc3b0198

fix: properly shutdown intake worker
When the intake worker shutsdown or fails, the Manager initiates a
shutdown. When the worker shuts down, it stops consuming on
the resource store subscription channel. The Kubernetes Controller,
which is sending updates on this channel when it is updating the
resource store, is blocked because there is no consumer anymore. This
prevents the controller from properly shutting down and timing
out.

Have the intake worker begin shutting down only when the subscription
channel is closed. This streamlines the shutdown sequence.

SIGTERM -> [controller,resource store exits] -> signals intake
worker to exit

Signed-off-by: Hamzah Qudsi <hamzah@antimetal.com>

internal/intake/worker.go
@@ -3,6 +3,7 @@ package intake
 import (
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/cenkalti/backoff/v5"
 
@@ -10,21 +11,12 @@ import (
 	intakev1 "github.com/antimetal/apis/gengo/service/resource/v1"
 	"github.com/go-logr/logr"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 const (
 	headerAuthorize = "authorization"
 )
-
-var retryableCodes = []codes.Code{
-	codes.Canceled,
-	codes.DeadlineExceeded,
-	codes.Unavailable,
-	codes.Aborted,
-}
 
 type worker struct {
 	apiKey string
@@ -71,65 +63,57 @@ func NewWorker(store resource.Store, opts ...WorkerOpts) (*worker, error) {
 }
 
 func (w *worker) Start(ctx context.Context) error {
-	eventSub := w.store.Subscribe(nil)
 loop:
-	for {
-		select {
-		case <-ctx.Done():
-			break loop
-		case event, ok := <-eventSub:
-			if !ok {
-				// resource store is closed so start the shutdown process
-				break loop
-			}
-			if w.stream == nil {
-				for {
-					_, err := backoff.Retry(ctx, func() (bool, error) {
-						streamCtx := metadata.NewOutgoingContext(
-							context.Background(), metadata.Pairs(headerAuthorize, fmt.Sprintf("bearer %s", w.apiKey)),
-						)
-						stream, err := w.client.Delta(streamCtx)
-						if err != nil {
-							w.logger.Error(err, "failed to create intake stream, retrying...")
-							return false, err
-						}
-						w.stream = stream
-						return true, nil
-					}, backoff.WithBackOff(backoff.NewExponentialBackOff()))
-					if err == nil {
-						break
+	for event := range w.store.Subscribe(nil) {
+		if w.stream == nil {
+			for {
+				_, err := backoff.Retry(ctx, func() (bool, error) {
+					streamCtx := metadata.NewOutgoingContext(
+						context.Background(), metadata.Pairs(headerAuthorize, fmt.Sprintf("bearer %s", w.apiKey)),
+					)
+					stream, err := w.client.Delta(streamCtx)
+					if err != nil {
+						w.logger.Error(err, "failed to create intake stream, retrying...")
+						return false, err
 					}
+					w.stream = stream
+					return true, nil
+				}, backoff.WithBackOff(backoff.NewExponentialBackOff()))
+
+				// Break out of the main loop if the context is canceled since that means
+				// we're shutting down. Continue on the main loop until the subscription channel
+				// is closed.
+				if err == nil || ctx.Err() == context.Canceled {
+					continue loop
 				}
-			}
-			w.logger.V(1).Info("sending intake stream", "eventType", event.Type, "numObjects", len(event.Objs))
-			err := w.stream.Send(&intakev1.DeltaRequest{
-				Deltas: []*intakev1.Delta{
-					{
-						Op:      eventTypeToOp(event.Type),
-						Objects: event.Objs,
-					},
-				},
-			})
-			if err != nil {
-				code := status.Code(err)
-				for _, c := range retryableCodes {
-					if code == c {
-						w.logger.Error(err, "failed to send to intake stream, reseting stream...")
-						// TODO: This is going to drop events on stream failure. We want to add
-						// back the event to the queue so that it can be retried when the stream
-						// is re-established.
-						w.stream = nil
-						continue loop
-					}
-				}
-				_, err = w.stream.CloseAndRecv()
-				w.logger.Error(err, "failed to send to intake stream")
-				return err
 			}
 		}
+
+		w.logger.V(1).Info("sending intake stream", "eventType", event.Type, "numObjects", len(event.Objs))
+		err := w.stream.Send(&intakev1.DeltaRequest{
+			Deltas: []*intakev1.Delta{
+				{
+					Op:      eventTypeToOp(event.Type),
+					Objects: event.Objs,
+				},
+			},
+		})
+		if err != nil {
+			// TODO: This is going to drop events on stream failure. We want to add
+			// back the event to the queue so that it can be retried when the stream
+			// is re-established.
+			if err != io.EOF {
+				w.logger.Error(err, "unexpected error sending to intake stream")
+			}
+			_, err = w.stream.CloseAndRecv()
+			if err != nil {
+				w.logger.Error(err, "failed to send to intake stream")
+			}
+			w.stream = nil
+		}
 	}
+
 	w.logger.Info("shutting down intake worker")
-	// Shutdown
 	if w.stream != nil {
 		if _, err := w.stream.CloseAndRecv(); err != nil {
 			return fmt.Errorf("failed to close intake stream: %w", err)

cmd/main.go
@@ -132,6 +132,10 @@ func main() {
 		setupLog.Error(err, "unable to create resource inventory")
 		os.Exit(1)
 	}
+	if err := mgr.Add(rsrcStore); err != nil {
+		setupLog.Error(err, "unable to register resource inventory")
+		os.Exit(1)
+	}
 
 	intakeConn, err := grpc.NewClient(intakeAddr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),

pkg/resource/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"bytes"
+	"context"
 	"crypto/sha256"
 	"fmt"
 	"reflect"
@@ -612,6 +613,14 @@ func (s *store) Close() error {
 	err := s.store.Close()
 	s.closed = true
 	return err
+}
+
+// Start implements the controller-runtime.Manager Runnable interface.
+// It blocks until ctx is done, at which point it will close the store in order
+// to clean up subscriptions.
+func (s *store) Start(ctx context.Context) error {
+	<-ctx.Done()
+	return s.Close()
 }
 
 func (s *store) startEventRouter() {

commit 758b3993bebb48159183ddc6b3a8b123be10dd19

fix: don't skip event when intake stream is created
During intake stream creation, it would continue on the main loop right
after creation, skipping the resource store event that triggered it.

Fixes: 7d6098a (fix: properly shutdown intake worker)
Signed-off-by: Hamzah Qudsi <hamzah@antimetal.com>

internal/intake/worker.go
@@ -80,10 +80,14 @@ loop:
 					return true, nil
 				}, backoff.WithBackOff(backoff.NewExponentialBackOff()))
 
+				if err == nil {
+					break
+				}
+
 				// Break out of the main loop if the context is canceled since that means
 				// we're shutting down. Continue on the main loop until the subscription channel
 				// is closed.
-				if err == nil || ctx.Err() == context.Canceled {
+				if ctx.Err() == context.Canceled {
 					continue loop
 				}
 			}