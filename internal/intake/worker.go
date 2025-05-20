e9ee44e73f690593376b81471aae13c32a988206
Hamzah Qudsi <hamzah@antimetal.com>
2025-05-20 16:06:14 -0400

    refactor: use queue in intake worker
    When the intake worker receives events from the resource store
    subscription, add the delta on a ratelimited queue. A separate goroutine
    is consuming from the queue to send to the intake service. If there are
    issues with gRPC intake service stream, the delta is added back to
    queue and the stream is reset. This prevents any potential loss when
    sending deltas to the intake service due to transient connection issues.
    
    Signed-off-by: Hamzah Qudsi <hamzah@antimetal.com>

--- internal/intake/worker.go
+++ internal/intake/worker.go
@@ -3,18 +3,23 @@ package intake
 import (
 	"context"
 	"fmt"
-	"io"
+	"strings"
+	"sync"
 
 	"github.com/cenkalti/backoff/v5"
+	"k8s.io/client-go/util/workqueue"
 
 	"github.com/antimetal/agent/pkg/resource"
 	intakev1 "github.com/antimetal/apis/gengo/service/resource/v1"
 	"github.com/go-logr/logr"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 const (
+	workerName      = "intake-worker"
 	headerAuthorize = "authorization"
 )
 
@@ -23,6 +28,7 @@ type worker struct {
 	client intakev1.IntakeServiceClient
 	store  resource.Store
 	logger logr.Logger
+	queue  workqueue.TypedRateLimitingInterface[*intakev1.Delta]
 
 	// runtime fields
 	stream intakev1.IntakeService_DeltaClient
@@ -52,78 +58,121 @@ func NewWorker(store resource.Store, opts ...WorkerOpts) (*worker, error) {
 	if store == nil {
 		return nil, fmt.Errorf("store can't be nil")
 	}
-	w := &worker{store: store}
+
+	ratelimiter := workqueue.DefaultTypedControllerRateLimiter[*intakev1.Delta]()
+	queue := workqueue.NewTypedRateLimitingQueueWithConfig(ratelimiter,
+		workqueue.TypedRateLimitingQueueConfig[*intakev1.Delta]{
+			Name: workerName,
+		},
+	)
+
+	w := &worker{
+		store: store,
+		queue: queue,
+	}
 	for _, opt := range opts {
 		opt(w)
 	}
+
 	if w.client == nil {
 		return nil, fmt.Errorf("can't create client")
 	}
 	return w, nil
 }
 
-func (w *worker) Start(ctx context.Context) error {
-loop:
+func (w *worker) Start(_ context.Context) error {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		w.streamer(ctx)
+	}()
+
 	for event := range w.store.Subscribe(nil) {
-		if w.stream == nil {
-			for {
-				_, err := backoff.Retry(ctx, func() (bool, error) {
-					streamCtx := metadata.NewOutgoingContext(
-						context.Background(), metadata.Pairs(headerAuthorize, fmt.Sprintf("bearer %s", w.apiKey)),
-					)
-					stream, err := w.client.Delta(streamCtx)
-					if err != nil {
-						w.logger.Error(err, "failed to create intake stream, retrying...")
-						return false, err
-					}
-					w.stream = stream
-					return true, nil
-				}, backoff.WithBackOff(backoff.NewExponentialBackOff()))
-
-				if err == nil {
-					break
-				}
-
-				// Break out of the main loop if the context is canceled since that means
-				// we're shutting down. Continue on the main loop until the subscription channel
-				// is closed.
-				if ctx.Err() == context.Canceled {
-					continue loop
-				}
-			}
-		}
-
-		w.logger.V(1).Info("sending intake stream", "eventType", event.Type, "numObjects", len(event.Objs))
-		err := w.stream.Send(&intakev1.DeltaRequest{
-			Deltas: []*intakev1.Delta{
-				{
-					Op:      eventTypeToOp(event.Type),
-					Objects: event.Objs,
-				},
-			},
+		w.queue.AddRateLimited(&intakev1.Delta{
+			Op:      eventTypeToOp(event.Type),
+			Objects: event.Objs,
 		})
-		if err != nil {
-			// TODO: This is going to drop events on stream failure. We want to add
-			// back the event to the queue so that it can be retried when the stream
-			// is re-established.
-			if err != io.EOF {
-				w.logger.Error(err, "unexpected error sending to intake stream")
-			}
-			_, err = w.stream.CloseAndRecv()
-			if err != nil {
-				w.logger.Error(err, "failed to send to intake stream")
-			}
-			w.stream = nil
-		}
 	}
 
 	w.logger.Info("shutting down intake worker")
-	if w.stream != nil {
-		if _, err := w.stream.CloseAndRecv(); err != nil {
-			return fmt.Errorf("failed to close intake stream: %w", err)
+	w.queue.ShutDownWithDrain()
+	cancel()
+	wg.Wait()
+	return nil
+}
+
+func (w *worker) streamer(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			if w.stream != nil {
+				if _, err := w.stream.CloseAndRecv(); err != nil {
+					w.logger.Error(err, "error closing intake stream")
+				}
+			}
+			return
+		default:
+			w.sendDelta(ctx)
 		}
 	}
-	return nil
+}
+
+func (w *worker) sendDelta(ctx context.Context) {
+	delta, shutdown := w.queue.Get()
+	if shutdown {
+		return
+	}
+	defer w.queue.Done(delta)
+
+	if w.stream == nil {
+		for {
+			_, err := backoff.Retry(ctx, func() (bool, error) {
+				streamCtx := metadata.NewOutgoingContext(
+					context.Background(), metadata.Pairs(headerAuthorize, fmt.Sprintf("bearer %s", w.apiKey)),
+				)
+				stream, err := w.client.Delta(streamCtx)
+				if err != nil {
+					w.logger.Error(err, "failed to create intake stream, retrying...")
+					return false, err
+				}
+				w.stream = stream
+				return true, nil
+			}, backoff.WithBackOff(backoff.NewExponentialBackOff()))
+
+			if err == nil {
+				break
+			}
+
+			// Return if the context is canceled since that means we're shutting down.
+			if ctx.Err() == context.Canceled {
+				return
+			}
+		}
+	}
+
+	w.logger.V(1).Info("sending delta", "op", delta.Op, "numObjects", len(delta.Objects))
+	err := w.stream.Send(&intakev1.DeltaRequest{Deltas: []*intakev1.Delta{delta}})
+	if err != nil {
+		_, err = w.stream.CloseAndRecv()
+		if err != nil {
+			code := status.Code(err)
+			if code == codes.Unavailable && strings.Contains(code.String(), "max_age") {
+				// Resetting stream due server max connection age
+				w.logger.V(1).Info("resetting intake stream")
+			} else {
+				w.logger.Error(err, "failed to send to intake stream")
+			}
+		}
+		w.stream = nil
+		if !w.queue.ShuttingDown() {
+			w.queue.AddRateLimited(delta)
+		}
+		return
+	}
+	w.queue.Forget(delta)
 }
 
 func eventTypeToOp(e resource.EventType) intakev1.DeltaOperation {
