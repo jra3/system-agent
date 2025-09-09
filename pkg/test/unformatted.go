6640cdb335b00426fe8487b8f719904d72b7cad8
John Allen <john@antimetal.com>
2025-09-09 14:28:49 -0400

    test: add unformatted Go code

---
+++ pkg/test/unformatted.go
@@ -0,0 +1,15 @@
+package test
+
+import "fmt"
+
+func   BadlyFormatted(  x    int,y string)string{
+fmt.Println("Bad formatting")
+    return    y
+}
+
+type  BadStruct   struct{
+Field1   string
+    Field2 int}
+
+func(b*BadStruct)Method()string{
+return b.Field1}
